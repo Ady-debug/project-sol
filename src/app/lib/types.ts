3f8a599f675d7ed9c971333b97de894cb79b7c67
Ady <adysoltan@gmail.com>
2026-07-20 17:00:33 +0100

    refactor: update ForecastItem type for accuracy; make ForecastCards component async to fetch on render

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,12 +1,15 @@
 export interface ForecastItem {
-  type: string;
+  type: "Sunrise" | "Sunset";
   model_data: boolean;
   quality: number;
   cloud_cover: number;
-  quality_text: string;
+  quality_text: "Poor" | "Fair" | "Good" | "Excellent";
   time: string;
   direction: number;
-  magics: object;
+  magics: {
+    blue_hour: string[];
+    golden_hour: string[];
+  };
 }
 
 export interface ForecastResponse {
