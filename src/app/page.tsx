3b54c5d0f81214028ed61514ad6048b3539fba7f
Ady <adysoltan@gmail.com>
2026-08-09 18:22:03 +0100

    refactor: improve JSX struture for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import ForecastDashboard from "@/components/forecast-dashboard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center m-25">
-      <main>
+    <main>
+      <div className="flex flex-col flex-1 items-center justify-center m-25">
         <ForecastDashboard />
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
