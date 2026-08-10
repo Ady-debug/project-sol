496b4494311ba7d6b5081c238c7e30000a826660
Ady <adysoltan@gmail.com>
2026-08-10 15:24:15 +0100

    feature: fix background to position to allow component overscroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="bg-[url('/mountains-bg.webp')] bg-cover bg-no-repeat bg-center min-h-screen">
+    <main className="bg-[url('/mountains-bg.webp')] bg-cover bg-no-repeat bg-fixed bg-position-[center_90px] min-h-screen">
       <Header />
       <ForecastDashboard />
     </main>
